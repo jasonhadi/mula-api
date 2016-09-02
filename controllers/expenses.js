a8f34903526bb238d9f5f90cdba7f43a2cd5f814
Jason Hadi <jhadi@rlsolutions.com>
2016-09-02 12:10:36 -0400

    proper async

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,11 +1,182 @@
 var mongoose = require('mongoose'), //mongo connection
     bodyParser = require('body-parser'), //parses information from POST
     methodOverride = require('method-override'), //used to manipulate POST
-    js2xmlparser = require('js2xmlparser'),
     mkdirp = require('mkdirp'),
+    moment = require('moment'),
+    async = require('async'),
     PDFMerge = require('pdf-merge'),
-    gm = require('gm').subClass({imageMagick: true});
+    gm = require('gm').subClass({imageMagick: true}),
+    Quixpense = require('../models/quixpense');
 
+mongoose.set('debug', true);
+
+function getExpense(req, res, next) {
+	Quixpense.Expense.findById(req.expenseid, '-sheet').populate('receipts').exec(function (err, expense) {
+		if (err) { return console.error(err); }
+		else { next(expense); }     
+	});
+}
+
+function getExpenses(req, res, next) {
+	Quixpense.Expense.find({ userId: req.params.userid }, '-sheet', function (err, expenses) {
+		if (err) { return console.error(err); }
+		else { next(expenses); }     
+	});
+}
+
+function deleteExpense(req, res, next) {
+	Quixpense.Expense.findById(req.expenseid, '-sheet', function (err, expense) {
+		if (err) {
+			console.log(req.expenseid + ' was not found');
+			res.status(500);
+			err = new Error('ID Not Found');
+			err.status = 500;
+			res.json({message : err.status  + ' ' + err});
+		} else {
+			expense.remove(function (err) {
+				if(err) {
+					console.log(req.expenseid + ' was not found');
+					res.status(500);
+					err = new Error('ID Not Found');
+					err.status = 500;
+					res.json({message : err.status  + ' ' + err});
+				} else {
+					res.json({
+						status: "success",
+						expense: expense
+					});
+				}
+			});
+		}
+	});
+}
+
+function getExpenseSheet(req, res, next) {
+   	Quixpense.Expense.findById(req.expenseid, 'sheet', function (err, expense) {
+		if (err) {
+			console.log(expenseid + ' was not found');
+			res.status(500);
+			err = new Error('ID Not Found');
+			err.status = 500;
+			res.json({message : err.status  + ' ' + err});
+		} else if(!expense) {
+			console.log(req.expenseid + ' was not found');
+			res.status(500);
+			err = new Error('ID Not Found');
+			err.status = 500;
+			res.json({message : err.status  + ' ' + err});
+		} else {
+			next(expense);
+		}
+	});
+}
+
+function numberExpenses(req, res, next) {
+	var activities = req.body.activities;
+	var expenseId = mongoose.Types.ObjectId();
+
+	Quixpense.Receipt.find({parentActivity: { $in: activities.map(mongoose.Types.ObjectId)}}, '-img')
+			 .sort({ parentActivity: 1 })
+			 .lean()
+		  	 .exec(function(err, receipts) {
+				var currentSheet = 1;
+				var currentActivity = 0;
+				var currentActivityId = "";
+				var receiptNumber = 1;
+				var oldestBillDate = moment();
+				var activitySave = false;
+				var activityArray = [];
+
+				async.each(receipts,
+					function(receipt, callback) {
+						if(currentActivityId != receipt.parentActivity) {
+							currentActivity++;
+							activitySave = true;
+							currentActivityId = receipt.parentActivity;
+							activityArray.push(receipt.parentActivity);
+						}
+						if(receiptNumber % 28 === 0 || currentActivity > 5) {
+							currentSheet++;
+							currentActivity = 1;
+							activitySave = true;
+						} 
+						if(oldestBillDate.isAfter(receipt.date)) oldestBillDate = moment(receipt.date);
+
+						if(activitySave) {
+							activitySave = false;
+
+							Quixpense.Activity.findById(currentActivityId, function (err, activity) {
+								activity.row.push({
+									sheetNumber: currentSheet,
+									number: currentActivity
+								});
+								activity.parentExpense = expenseId;
+								activity.save(function(err) {
+									Quixpense.Receipt.findById(receipt._id, '-img', function (err, receipt) {
+										receipt.sheetNumber = currentSheet;
+										receipt.receiptNumber = receiptNumber;
+										receipt.activityNumber = currentActivity;
+										receipt.parentExpense = expenseId;
+										receipt.save(function(err) {
+											callback();	
+										});
+									});
+								});
+							});
+						} else {
+							Quixpense.Receipt.findById(receipt._id, '-img', function (err, receipt) {
+								receipt.sheetNumber = currentSheet;
+								receipt.receiptNumber = receiptNumber;
+								receipt.activityNumber = currentActivity;
+								receipt.parentExpense = expenseId;
+								receipt.save(function(err) {
+									callback();	
+								});
+							});
+						}
+
+						receiptNumber++;
+					}, function(err) {
+						Quixpense.Activity.find({parentExpense: expenseId}, function(err, activities) {
+							generateExpense(expenseId, receipts, activities, next);
+						});
+					}
+				);
+			 });
+}
+
+function generateExpense(expenseId, newReceipts, newActivities, next) {
+	Quixpense.Expense.create({
+		_id: expenseId
+	}, function(err, expense) {
+		if(err) { console.log('sss'); }
+		expense.update({$push: {receipts: {$each: newReceipts}, activities: {$each: newActivities}}}, function(err, expense) {
+			if(err) { console.log('aaa'); }
+			next(expense);	
+		});
+	});
+}
+
+function updateActivitySheet(activityId, sheetNumber, activityNumber, expenseId) {
+	Quixpense.Activity.findById(activityId, function (err, activity) {
+		activity.row.push({
+			sheetNumber: sheetNumber,
+			number: activityNumber
+		});
+		activity.parentExpense = expenseId;
+		activity.save();
+	});
+}
+
+function updateReceiptSheet(receiptId, sheetNumber, activityNumber, receiptNumber, expenseId) {
+	Quixpense.Receipt.findById(receiptId, '-img', function (err, receipt) {
+		receipt.sheetNumber = sheetNumber;
+		receipt.receiptNumber = receiptNumber;
+		receipt.activityNumber = activityNumber;
+		receipt.parentExpense = expenseId;
+		receipt.save();
+	});
+}
 
 var sheetReceiptRowsCount = 27;
 var sheetActivityRowsCount = 5;
@@ -179,6 +350,9 @@ var clearExports = function(expenseId, cs, next) {
 
 
 module.exports = {
+	getExpense: getExpense,
+	generateExpense: generateExpense,
+	numberExpenses: numberExpenses,
 	getExpenses: function(req, res, next) {
 	        mongoose.model('Expense').find({}, function (err, expenses) {
 	              if (err) { return console.error(err); }
@@ -212,17 +386,17 @@ module.exports = {
 	        });
 	},
 	
-	verifyExpenseId: function(req, res, next, id) {
-	    mongoose.model('Expense').findById(id, function (err, expense) {
+	verifyExpenseId: function(req, res, next, expenseid) {
+	    mongoose.model('Expense').findById(expenseid, function (err, expense) {
 	        if (err) {
-	            console.log('Expense ' + id + ' was not found');
+	            console.log('Expense ' + expenseid + ' was not found');
 	            res.status(500);
 	            err = new Error('Expense ID Not Found');
 	            err.status = 500;
 		    res.json({message : err.status  + ' ' + err});
 	        } else {
 	            console.log(expense);
-	            req.id = id;
+	            req.expenseid = expenseid;
 	            next(); 
 	        } 
 	    });
@@ -241,18 +415,5 @@ module.exports = {
 
 	exportExpense: function(req, res, next) {
 		clearExports(req.id, calculateSheets, next);
-	},
-
-	exportExpenseXml: function( req, res, next) {
-	    mongoose.model('Expense').findById(req.id).exec( function (err, expense) {
-	      if (err) {
-	        console.log('GET Error: There was a problem retrieving: ' + err);
-	      } else {
-	        console.log('GET Retrieving ID: ' + expense._id);
-		var xml = js2xmlparser('expensesheet', JSON.stringify(expense));
-		console.log(xml);
-		next( xml );
-	      }
-	    });
 	}
 };
