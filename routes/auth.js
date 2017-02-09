3d45ac826929a1e6b194276c4e4f07751b823dec
Jason Hadi <jhadi@rlsolutions.com>
2017-02-09 13:39:26 -0500

    Remove temp admin nonsense.

--- routes/auth.js
+++ routes/auth.js
@@ -32,39 +32,25 @@ var jwt = require('jsonwebtoken');
  */
 module.exports = function(passport) {
 	router.post('/', function(req, res) {
-		if(req.body.username === 'admin') {
-			userController.getUserByUsername('admin', function(user) {
-				var userObj = {
-					id: user._id,
-					username: 'admin',
-					firstname: 'Admin',
-					lastname: 'User',
-					email: 'jhadi@rlsolutions.com',
+		passport.authenticate('ldapauth', {session: false})(req, res, function(err) {
+			if (err) { return res.status(500).json(err); }
+			if (!req.user) { return res.status(401).json({ status: 'error', code: 'UNAUTHORIZED' }); } 
+			else { 
+				userController.getUserByUsername(req.user.sAMAccountName, function(user) {
+					var userObj = {
+						id: user._id,
+					username: req.user.sAMAccountName,
+					firstname: req.user.givenName,
+					lastname: req.user.sn,
+					email: req.user.mail,
 					expCurrency: user.expCurrency,
 					reimbCurrency: user.reimbCurrency
-				};
-				return res.json({ token: jwt.sign(userObj, config.jwtsecret) }); 
-			});
-		} else {
-			passport.authenticate('ldapauth', {session: false})(req, res, function(err) {
-				if (err) { return res.status(500).json(err); }
-				if (!req.user) { return res.status(401).json({ status: 'error', code: 'UNAUTHORIZED' }); } 
-				else { 
-					userController.getUserByUsername(req.user.sAMAccountName, function(user) {
-						var userObj = {
-							id: user._id,
-							username: req.user.sAMAccountName,
-							firstname: req.user.givenName,
-							lastname: req.user.sn,
-							email: req.user.mail,
-							expCurrency: user.expCurrency,
-							reimbCurrency: user.reimbCurrency
-						};
-						return res.json({ token: jwt.sign(userObj, config.jwtsecret) }); 
-					});
-				}
-			});
-		}
+					};
+					return res.json({ token: jwt.sign(userObj, config.jwtsecret) }); 
+				});
+			}
+		});
+
 	});
 	return router;
 };
